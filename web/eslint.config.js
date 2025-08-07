c23aa54869c59055992733f01b03e2bdb51e03a6
lvjialei <1241597635@qq.com>
2025-08-07 10:53:34 +0800

    feat: 添加 TypeScript 支持到 ESLint 配置中，并更新相关依赖

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -20,5 +20,15 @@ export default defineConfig(
       ],
       'unocss/order-attributify': 'off'
     }
+  },
+  // 引入 @typescript-eslint/parser
+  {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
+    },
+    plugins: ['@typescript-eslint'],
+    extends: ['plugin:@typescript-eslint/recommended'],
   }
 );
